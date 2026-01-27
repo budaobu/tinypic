620fd254ca80b918344bfa57d5eade1461e6f5ff
bufang <1931297+budaobu@users.noreply.github.com>
2026-01-27 15:36:12 +0800

    SSG

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
+import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
+import Home from './views/Home.vue'
+import './style.css'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: Home }
+]
+
+export const createApp = ViteSSG(
+  App,
+  { routes },
+  ({ app, router, routes, isClient, initialState }) => {
+    // Custom setup hooks can go here
+  },
+)
